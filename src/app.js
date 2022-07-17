77de239062c3c3924e348f6b870b694faf27d66c
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-07-17 18:44:00 +0300

    feat: handlebars & scss

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('Hello world!');
+// const Handlebars = require("handlebars");
+
+// var source = document.getElementById("entry-template").innerHTML;
+// var template = Handlebars.compile(source);
+
+// var context = { title: "My New Post", body: "This is my first post!" };
+// var html = template(context);
