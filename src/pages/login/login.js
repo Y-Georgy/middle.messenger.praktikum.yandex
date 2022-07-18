5f6ed20cb8921c8a49c6f75e5643134f51472574
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-07-19 02:26:31 +0300

    feat: login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,13 @@
-import { authTitle } from "../../components/authTitle/authTitle";
-import { authLayout } from "../../layouts/auth/auth";
+import { authTitle } from '../../components/authTitle/authTitle';
+import { authLayout } from '../../layouts/auth/auth';
+import { authInput } from '../../components/authInput/authInput';
 
 const container = document.querySelector('.main');
 const title = authTitle('Вход');
-const authSection = authLayout(title);
+const loginInput = authInput('login', 'Логин', 'Иванов', 'Неверный логин');
+const passwordInput = authInput('password', 'Пароль','Иванов', '', 'password');
+
+const content = title + loginInput + passwordInput;
+const authSection = authLayout(content);
+
 container.innerHTML = authSection;
