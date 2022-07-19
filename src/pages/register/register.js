5316fa61d0bee1f0d4212672ac4050a3921a7790
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-07-19 16:22:42 +0300

    feat: register form

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -3,11 +3,12 @@ import { authLayout } from '../../layouts/auth/auth';
 import { authInput } from '../../components/authInput/authInput';
 import { buttonSubmit } from '../../components/buttonSubmit/buttonSubmit';
 import { link } from '../../components/link/link';
+import { form } from '../../components/form/form';
 
 const container = document.querySelector('.main');
 const title = authTitle('Вход');
 
-const emailInput = authInput('email', 'Почта', 'pochta@yandex.ru', '');
+const emailInput = authInput('email', 'Почта', 'pochta@yandex.ru', '', 'email');
 const loginInput = authInput('login', 'Логин', 'ivanivanov', '');
 const firstNameInput = authInput('first_name', 'Имя', 'Иван', '');
 const secondNameInput = authInput('second_name', 'Фамилия', 'Иванов', '');
@@ -16,9 +17,13 @@ const passwordInput = authInput('password', 'Пароль','12345678', '', 'pass
 const passwordCheckInput = authInput('password-check', 'Пароль (еще раз)','12345678', 'Пароли не совпадают', 'password');
 
 const button = buttonSubmit('Зарегистрироваться');
+
+const formContent = emailInput + loginInput + firstNameInput + secondNameInput + phoneInput + passwordInput + passwordCheckInput + button;
+const registerForm = form('register', formContent)
+
 const registerLink = link('login', 'Войти')
 
-const content = title + emailInput + loginInput + firstNameInput + secondNameInput + phoneInput + passwordInput + passwordCheckInput + button + registerLink;
+const content = title + registerForm + registerLink;
 const authSection = authLayout(content);
 
 container.innerHTML = authSection;
