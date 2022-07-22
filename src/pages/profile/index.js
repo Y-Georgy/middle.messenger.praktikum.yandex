280c49d25c78cc8a781d9b4748a7fe9f820065b3
Yadrihinskiy Georgy <yadrihinskiy.georgy@uep24.ru>
2022-07-22 17:51:34 +0300

    feat: profile avatar

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -2,10 +2,11 @@ import Handlebars from 'handlebars';
 import profileLayout from "../../layouts/profile";
 import avatar from "../../components/avatar";
 import profile from "./profile.hbs"
+import styles from "./profile.module.scss"
 
 Handlebars.registerPartial('avatar', avatar);
 
-const profilePage = profileLayout(profile);
+const profilePage = profileLayout(profile({styles}));
 // const profilePage = profileLayout(profile);
 
 export default profilePage;
