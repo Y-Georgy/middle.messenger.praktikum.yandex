2f2643a50af62c4df8f105f8a9e5793f13ad33b3
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-07-22 21:48:42 +0300

    refactor: profile

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,14 +1,14 @@
 import Handlebars from 'handlebars';
 import profileLayout from "../../layouts/profile";
 import avatar from "../../components/avatar";
-import profile from "./profile.hbs"
+import template from "./template.hbs";
 import input from '../../components/profileInput';
-import styles from "./profile.module.scss"
+import styles from "./styles.module.scss";
 
 Handlebars.registerPartial('avatar', avatar);
 Handlebars.registerPartial('input', input);
 
-const profilePage = profileLayout(profile({styles}));
+const profilePage = profileLayout(template({styles}));
 // const profilePage = profileLayout(profile);
 
 export default profilePage;
