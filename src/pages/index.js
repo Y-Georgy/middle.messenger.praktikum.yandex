7a770378db21d93b2bed85dca80be31c57e855e9
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-07-24 23:27:03 +0300

    feat: 404

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import login from "./login";
 import register from "./register"
 import profile from "./profile"
 import changePassword from "./changePassword";
+import page404 from "./404";
 
 const path = window.location.pathname;
 const container = document.querySelector("#main");
@@ -10,3 +11,5 @@ if (path === "/login") container.innerHTML = login;
 else if (path === "/register") container.innerHTML = register;
 else if (path === "/profile") container.innerHTML = profile;
 else if (path === "/change-password") container.innerHTML = changePassword;
+// else if (path === "/505") container.innerHTML = page505;
+else container.innerHTML = page404;
