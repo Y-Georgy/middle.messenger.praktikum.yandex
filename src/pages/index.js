98d18747d1a44a035eabe9318ca641a980798d39
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-07-24 23:29:25 +0300

    feat: 500

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import login from "./login";
 import register from "./register"
 import profile from "./profile"
 import changePassword from "./changePassword";
+import page500 from "./500";
 import page404 from "./404";
 
 const path = window.location.pathname;
@@ -11,5 +12,5 @@ if (path === "/login") container.innerHTML = login;
 else if (path === "/register") container.innerHTML = register;
 else if (path === "/profile") container.innerHTML = profile;
 else if (path === "/change-password") container.innerHTML = changePassword;
-// else if (path === "/505") container.innerHTML = page505;
+else if (path === "/500") container.innerHTML = page500;
 else container.innerHTML = page404;
