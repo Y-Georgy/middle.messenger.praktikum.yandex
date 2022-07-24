2ba5766a47b2d90a925b58c3b15e0335a2b00e03
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-07-24 23:04:43 +0300

    feat: popup

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -3,12 +3,19 @@ import profileLayout from "../../layouts/profile";
 import avatar from "../../components/avatar";
 import template from "./template.hbs";
 import input from '../../components/profileInput';
+import button from '../../components/buttonSubmit';
+import popup from "../../components/popup"
+import popupContent from "./popupContent.hbs"
 import styles from "./styles.module.scss";
+import popupStyles from "./popupStyles.module.scss";
+
 
 Handlebars.registerPartial('avatar', avatar);
 Handlebars.registerPartial('input', input);
+Handlebars.registerPartial('popup', popup);
 
-const profilePage = profileLayout(template({styles}));
-// const profilePage = profileLayout(profile);
+const styledPopupContent = popupContent({ styles: popupStyles});
+
+const profilePage = profileLayout(template({styles, styledPopupContent}));
 
 export default profilePage;
