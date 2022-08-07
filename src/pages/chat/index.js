fec13d92ccca62981db13547c0f62c5d5c12dcbb
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-08-07 16:05:13 +0300

    feat: recipient начало

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,8 +1,8 @@
 import Handlebars from "handlebars";
 import template from "./template.hbs";
-import link from "../../components/link";
+import recipient from "../../components/recipient";
 import * as styles from "./styles.module.scss";
 
-Handlebars.registerPartial("link", link);
-
+Handlebars.registerPartial("recipient", recipient);
+// { name: "Андрей", avatarLink: '', lastMessage: { text: '', isMy: false, time: '10:21' }, unreadCount: 2 }
 export default template({ styles });
