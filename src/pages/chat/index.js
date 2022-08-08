440ca374cbe3454ba11216402c116e8ebe0631e2
Yadrihinskiy Georgy <yadrihinskiy.georgy@uep24.ru>
2022-08-08 12:09:21 +0300

    feat: chat currentRecipent

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -7,6 +7,7 @@ Handlebars.registerPartial("recipient", recipient);
 const recipients = [
     { name: "Андрей", avatarLink: 'https://sun1-97.userapi.com/s/v1/if1/Y_wWjvbJ8Erwfj0N_VQ0VhiJ5KQO1UJTwVaUcY72SkXJEAUAk4IHSFGKjjl-1OtVlPMShw.jpg?size=200x200&quality=96&crop=1,1,1339,1339&ava=1', lastMessage: { text: 'Текст сообщения', isMy: false, time: '10:21' }, unreadCount: 2 },
     { name: "Андрей 2", avatarLink: '', lastMessage: { text: 'Текст очень и очень длинного сообщения. Текст очень и очень длинного сообщения', isMy: true, time: '10:22' }, unreadCount: 2 },
-    { name: "Андрей", avatarLink: 'https://sun1-97.userapi.com/s/v1/if1/Y_wWjvbJ8Erwfj0N_VQ0VhiJ5KQO1UJTwVaUcY72SkXJEAUAk4IHSFGKjjl-1OtVlPMShw.jpg?size=200x200&quality=96&crop=1,1,1339,1339&ava=1', lastMessage: { text: 'Текст сообщения', isMy: false, time: '10:21' }, unreadCount: 2 }
+    { name: "Андрей", avatarLink: 'https://sun1-97.userapi.com/s/v1/if1/Y_wWjvbJ8Erwfj0N_VQ0VhiJ5KQO1UJTwVaUcY72SkXJEAUAk4IHSFGKjjl-1OtVlPMShw.jpg?size=200x200&quality=96&crop=1,1,1339,1339&ava=1', lastMessage: { text: 'Текст сообщения', isMy: false, time: '10:21' }, unreadCount: 0 }
 ];
-export default template({ styles, recipients });
+const currentRecipent = { name: "Андрей", avatarLink: 'https://sun1-97.userapi.com/s/v1/if1/Y_wWjvbJ8Erwfj0N_VQ0VhiJ5KQO1UJTwVaUcY72SkXJEAUAk4IHSFGKjjl-1OtVlPMShw.jpg?size=200x200&quality=96&crop=1,1,1339,1339&ava=1', messages: [{ text: 'Текст сообщения', isMy: false, time: '10:21' }] }
+export default template({ styles, recipients, currentRecipent });
