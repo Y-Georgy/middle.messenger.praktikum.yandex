72ee9dd4c1b4e82086d4a49583bf6ea122f36d2f
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-08-16 20:11:45 +0300

    feat: Popup types

--- src/pages/profile/popupContent/index.ts
+++ src/pages/profile/popupContent/index.ts
@@ -1,9 +1,13 @@
 import template from "./template.hbs";
 import Component from "../../../utils/Component"
+import ButtonSubmit from "../../../components/buttonSubmit";
 import * as styles from "./styles.module.scss";
 
+type TProps = {
+  button: ButtonSubmit
+}
 class PopupContent extends Component {
-  constructor(props) {
+  constructor(props: TProps) {
     super(props, "div");
   }
 
