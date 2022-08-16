f0309e02a525c7321949ab427157b7a4feb625d2
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-08-16 09:57:15 +0300

    style: colors variables

--- src/components/authInput/index.ts
+++ src/components/authInput/index.ts
@@ -2,16 +2,6 @@ import template from "./template.hbs";
 import Component from "../../utils/Component"
 import * as styles from "./styles.module.scss";
 
-type TProps = {
-  errorText: string,
-  name: string,
-  type: string,
-  value: string,
-  label: string,
-  styles: {
-    input:string
-  }
-};
 
 class Input extends Component {
   constructor(props) {
