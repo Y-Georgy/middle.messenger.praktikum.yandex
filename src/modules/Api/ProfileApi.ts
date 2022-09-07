4c554c65d013ed6dfd1c7480aa7041b1497f95e8
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-09-07 22:56:19 +0300

    feat: ProfileApi

---
+++ src/modules/Api/ProfileApi.ts
@@ -0,0 +1,63 @@
+import { HTTPTransport } from "./Api";
+
+type TOptionsProfileAPI = {
+  baseUrl: string,
+  headers: {
+    'Accept': string
+    'Content-Type': string
+  }
+}
+
+export type TProfileValues = {
+  first_name: string,
+  second_name: string,
+  login: string,
+  email: string,
+  display_name: string,
+  phone: string
+}
+
+class ProfileApi extends HTTPTransport {
+  _baseUrl: string;
+  _headers: {
+    'Accept': string
+    'Content-Type': string
+  };
+
+  constructor({ baseUrl, headers }: TOptionsProfileAPI) {
+    super();
+    this._baseUrl = baseUrl
+    this._headers = headers
+  }
+
+  _handleResponse(res: {status: number, responseText: string}) {
+    if (res.status === 200) {
+      try {
+        return JSON.parse(res.responseText)
+      } catch (e) {
+        return res.responseText;
+      }
+    }
+
+    const errText: string | undefined = JSON.parse(res.responseText).reason;
+    return Promise.reject(errText ? errText : `Произошла ошибка ${res.status}`)
+  }
+
+  changeUserProfile(profileValues: TProfileValues) {
+    return this.put(
+      `${this._baseUrl}/api/v2/user/profile`,
+      {
+        headers: this._headers,
+        data: profileValues
+      }
+    ).then(this._handleResponse)
+  }
+}
+
+export const profileApi = new ProfileApi({
+  baseUrl: "https://ya-praktikum.tech",
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+});
